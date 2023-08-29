38cfa8bad61d3dc45ae908ea71c5ebf2f929548d
Lucas Santana <santanafx@hotmail.com>
2023-08-29 15:50:35 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       int numero = 1234;
+       System.out.println(numero);
+       int num = Integer.valueOf(numero);
+       System.out.println(num);
+       Integer num1 = num;
+       System.out.println(num1);
+    }
+}
